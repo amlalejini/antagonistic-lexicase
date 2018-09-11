482fca4f5b9f075c5bfe40b9daab60fec12976b1
Alex Lalejini <amlalejini@gmail.com>
2018-09-11 14:11:21 -0400

    Initial implementation of direct encoded sorting network organism along with initial test.

--- experiment/source/SortingNetworkOrg.h
+++ experiment/source/SortingNetworkOrg.h
@@ -3,23 +3,9 @@
 
 #include "base/array.h"
 #include "base/vector.h"
+#include "tools/Random.h"
 
 class SortingNetworkOrg {
-public:
-
-protected:
-  size_t input_size;
-
-public:
-
-  SortingNetworkOrg(size_t _input_size=4) 
-    : input_size(_input_size) { ; }
-
-  size_t GetInputSize() const { return input_size; }
-
-};
-
-class SortingNetworkOrg_DE : public SortingNetworkOrg {
 public:
   using genome_t = emp::vector<emp::array<size_t, 2>>;
 
@@ -28,11 +14,69 @@ protected:
   genome_t genome;
 
 public:
+  
+  SortingNetworkOrg(emp::Random & rnd, size_t input_size, size_t min_network_size, size_t max_network_size)
+    : genome()
+  { RandomizeGenome(rnd, input_size, min_network_size, max_network_size); }
 
-  SortingNetworkOrg_DE(size_t _input_size=4)
-    : SortingNetworkOrg(_input_size), genome() 
+  SortingNetworkOrg(const genome_t & _g)
+    : genome(_g)
   { ; }
 
+  size_t GetSize() const { return genome.size(); }
+
+  genome_t & GetGenome() { return genome; }
+  
+  void SetGenome(const genome_t & in) { genome = in; }
+
+  void RandomizeGenome(emp::Random & rnd, size_t input_size, size_t network_size);
+  void RandomizeGenome(emp::Random & rnd, size_t input_size, size_t min_network_size, size_t max_network_size);
+
+  bool ValidateNetwork(size_t input_size, size_t min_network_size=0, size_t max_network_size=(size_t)-1) const;
+
+  void Print(std::ostream & out=std::cout) const;
+  void PrintVert(std::ostream & out=std::cout) const;
+
 };
+
+void SortingNetworkOrg::RandomizeGenome(emp::Random & rnd, size_t input_size, 
+                                        size_t network_size) {
+  genome.resize(network_size);
+  for (size_t i = 0; i < genome.size(); ++i) {
+    genome[i][0] = rnd.GetUInt(0, input_size);
+    genome[i][1] = rnd.GetUInt(0, input_size);
+  }
+}
+
+void SortingNetworkOrg::RandomizeGenome(emp::Random & rnd, size_t input_size,
+                                        size_t min_network_size, size_t max_network_size) {
+  const size_t s = rnd.GetUInt(min_network_size, max_network_size+1);
+  RandomizeGenome(rnd, input_size, s);
+}
+
+bool SortingNetworkOrg::ValidateNetwork(size_t input_size, size_t min_network_size, 
+                                        size_t max_network_size) const {
+  if (GetSize() > max_network_size || GetSize() < min_network_size) return false;
+  for (size_t i = 0; i < genome.size(); ++i) {
+    if (genome[i][0] >= input_size || genome[i][1] >= input_size) return false;
+  }
+  return true;
+}
+
+void SortingNetworkOrg::Print(std::ostream & out) const {
+  out << "[";
+  for (size_t i=0; i < genome.size(); ++i) {
+    if (i) out << ", ";
+    out << "(" << genome[i][0] << "=>" << genome[i][1] << ")";
+  }
+  out << "]";
+}
+
+void SortingNetworkOrg::PrintVert(std::ostream & out) const {
+  for (size_t i=0; i < genome.size(); ++i) {
+    if (i) out << "\n";
+    out << genome[i][0] << " => " << genome[i][1];
+  }
+} 
 
 #endif
--- experiment/source/tests/test_sorting_network.cc
+++ experiment/source/tests/test_sorting_network.cc
@@ -1,17 +1,45 @@
 #define CATCH_CONFIG_MAIN
 #include "third-party/Catch/single_include/catch.hpp"
 
+#include <iostream>
+
+#include "base/Ptr.h"
 #include "base/vector.h"
 #include "tools/Random.h"
 
 #include "SortingNetworkOrg.h"
 
-TEST_CASE("SortingNetworkOrg_DE", "[sorting_network]") {
+/*
+
+To test:
+
+- [ ] Validation function (handwrite some good/bad genomes)
+- [ ] Copy constructor/genome constructor
+
+*/
+
+TEST_CASE("SortingNetworkOrg", "[sorting_network]") {
  
-  using org_t = SortingNetworkOrg_DE;
+  using org_t = SortingNetworkOrg;
 
-  org_t network_org(4);
+  constexpr int seed = 2;
 
-  REQUIRE(network_org.GetInputSize() == 4);
+  emp::Ptr<emp::Random> random = emp::NewPtr<emp::Random>(seed);
+
+  std::cout << "Building a random network (n=16, minsize=8, maxsize=16)." << std::endl;
+  org_t network0(*random, 16, 8, 16);
+
+  std::cout << "Network 0 (size=" << network0.GetSize() << "): ";
+  network0.Print();
+  std::cout << std::endl;
+  REQUIRE(network0.ValidateNetwork(16, 8, 16));
+
+  // Generate many networks, validate.
+  for (size_t i = 0; i < 1000; ++i) {
+    org_t org(*random, 64, 0, 1024);
+    REQUIRE(org.ValidateNetwork(64, 0, 1024));
+  }
+
+  random.Delete();
 
 }
