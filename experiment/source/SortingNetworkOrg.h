13055ab732c53bd053ab3a64e831e7a6492788c3
Alex Lalejini <amlalejini@gmail.com>
2018-09-11 15:38:40 -0400

    Separated sorting network from sorting network organism. Updated sorting network test appropriately.

--- experiment/source/SortingNetworkOrg.h
+++ experiment/source/SortingNetworkOrg.h
@@ -5,9 +5,11 @@
 #include "base/vector.h"
 #include "tools/Random.h"
 
+#include "SortingNetwork.h"
+
 class SortingNetworkOrg {
 public:
-  using genome_t = emp::vector<emp::array<size_t, 2>>;
+  using genome_t = SortingNetwork;
 
 protected:
 
@@ -23,60 +25,21 @@ public:
     : genome(_g)
   { ; }
 
-  size_t GetSize() const { return genome.size(); }
+  size_t GetSize() const { return genome.GetSize(); }
 
   genome_t & GetGenome() { return genome; }
   
   void SetGenome(const genome_t & in) { genome = in; }
 
-  void RandomizeGenome(emp::Random & rnd, size_t input_size, size_t network_size);
-  void RandomizeGenome(emp::Random & rnd, size_t input_size, size_t min_network_size, size_t max_network_size);
-
-  bool ValidateNetwork(size_t input_size, size_t min_network_size=0, size_t max_network_size=(size_t)-1) const;
+  void RandomizeGenome(emp::Random & rnd, size_t input_size, size_t network_size) { genome.RandomizeNetwork(rnd, input_size, network_size); };
+  void RandomizeGenome(emp::Random & rnd, size_t input_size, size_t min_network_size, size_t max_network_size) { genome.RandomizeNetwork(rnd, input_size, min_network_size, max_network_size); };
 
   void Print(std::ostream & out=std::cout) const;
-  void PrintVert(std::ostream & out=std::cout) const;
 
 };
 
-void SortingNetworkOrg::RandomizeGenome(emp::Random & rnd, size_t input_size, 
-                                        size_t network_size) {
-  genome.resize(network_size);
-  for (size_t i = 0; i < genome.size(); ++i) {
-    genome[i][0] = rnd.GetUInt(0, input_size);
-    genome[i][1] = rnd.GetUInt(0, input_size);
-  }
-}
 
-void SortingNetworkOrg::RandomizeGenome(emp::Random & rnd, size_t input_size,
-                                        size_t min_network_size, size_t max_network_size) {
-  const size_t s = rnd.GetUInt(min_network_size, max_network_size+1);
-  RandomizeGenome(rnd, input_size, s);
-}
 
-bool SortingNetworkOrg::ValidateNetwork(size_t input_size, size_t min_network_size, 
-                                        size_t max_network_size) const {
-  if (GetSize() > max_network_size || GetSize() < min_network_size) return false;
-  for (size_t i = 0; i < genome.size(); ++i) {
-    if (genome[i][0] >= input_size || genome[i][1] >= input_size) return false;
-  }
-  return true;
-}
 
-void SortingNetworkOrg::Print(std::ostream & out) const {
-  out << "[";
-  for (size_t i=0; i < genome.size(); ++i) {
-    if (i) out << ", ";
-    out << "(" << genome[i][0] << "=>" << genome[i][1] << ")";
-  }
-  out << "]";
-}
-
-void SortingNetworkOrg::PrintVert(std::ostream & out) const {
-  for (size_t i=0; i < genome.size(); ++i) {
-    if (i) out << "\n";
-    out << genome[i][0] << " => " << genome[i][1];
-  }
-} 
 
 #endif
--- experiment/source/tests/test_sorting_network.cc
+++ experiment/source/tests/test_sorting_network.cc
@@ -7,7 +7,7 @@
 #include "base/vector.h"
 #include "tools/Random.h"
 
-#include "SortingNetworkOrg.h"
+#include "SortingNetwork.h"
 
 /*
 
@@ -18,26 +18,40 @@ To test:
 
 */
 
-TEST_CASE("SortingNetworkOrg", "[sorting_network]") {
+TEST_CASE("SortingNetwork", "[sorting_network]") {
  
-  using org_t = SortingNetworkOrg;
+  using network_t = SortingNetwork;
 
   constexpr int seed = 2;
 
   emp::Ptr<emp::Random> random = emp::NewPtr<emp::Random>(seed);
 
   std::cout << "Building a random network (n=16, minsize=8, maxsize=16)." << std::endl;
-  org_t network0(*random, 16, 8, 16);
+  network_t network0(*random, 16, 8, 16);
 
   std::cout << "Network 0 (size=" << network0.GetSize() << "): ";
   network0.Print();
   std::cout << std::endl;
-  REQUIRE(network0.ValidateNetwork(16, 8, 16));
+  REQUIRE(network0.Validate(16, 8, 16));
 
   // Generate many networks, validate.
   for (size_t i = 0; i < 1000; ++i) {
-    org_t org(*random, 64, 0, 1024);
-    REQUIRE(org.ValidateNetwork(64, 0, 1024));
+    network_t network(*random, 64, 1, 1024);
+    REQUIRE(network.Validate(64, 1, 1024));
+
+    // Copy network
+    network_t network_cp(network);
+    REQUIRE(network == network_cp);
+
+    // Modify original network
+    network[0][0] = 512; network[0][1] = 1024;
+    REQUIRE(network.GetNetwork()[0][0] == 512);
+    REQUIRE(network.GetNetwork()[0][1] == 1024);
+    REQUIRE(network != network_cp);
+    REQUIRE(network.Validate(64, 1, 1024) == false);
+    // Break network_cp
+    network_cp.GetNetwork().resize(2048);
+    REQUIRE(network.Validate(64, 1, 1024) == false);
   }
 
   random.Delete();
