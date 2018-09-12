575ca98463bc041de4543ffe279c47b9b68892b0
Alex Lalejini <amlalejini@gmail.com>
2018-09-12 14:56:13 -0400

    Added more config options to sorting network config.

--- experiment/source/SortingNetworkConfig.h
+++ experiment/source/SortingNetworkConfig.h
@@ -5,7 +5,13 @@
 
 EMP_BUILD_CONFIG(SortingNetworkConfig, 
   GROUP(DEFAULT_GROUP, "General settings"),
-  VALUE(PLACEHOLDER, size_t, 0, "placeholder settings")
+  VALUE(SEED, int, 0, "Random number seed (-1 for based on time)"),
+  VALUE(MAX_NETWORK_SIZE, size_t, 128, "Maximum size of a sorting network"),
+  VALUE(MIN_NETWORK_SIZE, size_t, 1, "Minimum size of a sorting network"),
+  VALUE(TEST_SEQ_SIZE, size_t, 16, "Size of sequences being sorted by sorting networks"),
+  VALUE(SEQ_PER_TEST, size_t, 1, "How many test sequences are there per test [org]?"),
+  VALUE(NETWORK_POP_SIZE, size_t, 1000, "Population size for sorting networks"),
+  VALUE(TEST_POP_SIZE, size_t, 1000, "Population size for sorting tests - how many tests exist at once?")
 )
 
 #endif
