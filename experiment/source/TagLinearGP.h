6900e27bcd128b5a792cbddfaf0f31e02ec67164
Alex Lalejini <amlalejini@gmail.com>
2018-09-28 13:31:54 -0400

    TagLGP in progress. Fixed current version's compiler errors.

--- experiment/source/TagLinearGP.h
+++ experiment/source/TagLinearGP.h
@@ -10,7 +10,7 @@ namespace TagLGP {
   /// Instruction library class used by TagLinearGP class.
   /// Original version pulled from InstLib.h in Empirical library.
   /// - Modified to potentially facilitate some experimental functionality.
-  template<typename HARDWARE_T, size_t MAX_ARG_CNT=3>
+  template<typename HARDWARE_T>
   class InstLib {
   public:
     using hardware_t = HARDWARE_T;
@@ -128,11 +128,13 @@ namespace TagLGP {
 
     using module_t = Module;
     using inst_t = Instruction;
+    using inst_lib_t = InstLib<hardware_t>;
+
     using program_t = Program;
-    
-    // static constexpr size_t DEFAULT_MAX_CALL_DEPTH = 128;
-    // static constexpr mem_val_t DEFAULT_MEM_VALUE = 0.0;
-    // static constexpr double DEFAULT_MIN_BIND_THRESH = 0.5;
+
+    static constexpr size_t DEFAULT_MEM_SIZE = 16;
+    static constexpr size_t DEFAULT_MAX_CALL_DEPTH = 128;
+    static constexpr double DEFAULT_MIN_TAG_SPECIFICITY = 0.0;
 
     /// Struct - State
     /// - Description: maintains information about local call state.
@@ -185,7 +187,7 @@ namespace TagLGP {
       memory_t & GetInputMem() { return input_mem; }
       memory_t & GetOutputMem() { return output_mem; }
 
-      mem_pos_t & GetWorkingPos(size_t i) 
+      // mem_pos_t & GetWorkingPos(size_t i) { ; }
 
       void SetMP(size_t mp) { mptr = mp;}
       void SetIP(size_t ip) { iptr = ip; }
@@ -224,10 +226,10 @@ namespace TagLGP {
         }
 
       public:
-        MemoryValue(double _default_num=0, const std::string & _default_str="") 
+        MemoryValue() 
           : type(MemoryType::NUM), 
-            default_str(_default_str), 
-            default_num(_default_num),
+            default_str(""), 
+            default_num(0),
             num(default_num) 
         { ; }
 
@@ -287,6 +289,7 @@ namespace TagLGP {
           if (type == MemoryType::NUM) {
             return num;
           } else {
+            // TODO - Can str be converted to number?
             return default_num;
           }
         }
@@ -328,10 +331,76 @@ namespace TagLGP {
     };
 
     struct Instruction {
+      size_t id;
+      emp::vector<tag_t> arg_tags;
+
+      Instruction(size_t _id=0, const emp::vector<tag_t> & _arg_tags=emp::vector<tag_t>())
+        : id(_id), arg_tags(_arg_tags) { ; }
+      
+      Instruction(const Instruction &) = default;
+      Instruction(Instruction &&) = default;
+
+      Instruction & operator=(const Instruction &) = default;
+      Instruction & operator=(Instruction &&) = default;
+
+      bool operator==(const Instruction & in) const {
+        return id == in.id && arg_tags == in.arg_tags;
+      }
+      bool operator!=(const Instruction & in) const { return !(*this == in); }
+
+      bool operator<(const Instruction & other) const {
+          return std::tie(id, arg_tags) < std::tie(other.id, other.arg_tags);
+      }
+
+      size_t GetNumArgs() const { return arg_tags.size(); }
+
+      emp::vector<tag_t> & GetArgTags() {
+        return arg_tags;
+      }
+
+      tag_t & GetArgTag(size_t i) {
+        emp_assert(i < arg_tags.size());
+        return arg_tags[i];
+      }
 
     };
 
     struct Program {
+      using inst_seq_t = emp::vector<Instruction>;
+
+      emp::Ptr<const inst_lib_t> inst_lib;  ///< Pointer to instruction library associated with this program.
+      inst_seq_t program;     ///< Programs are linear sequences of instructions.
+
+      Program(emp::Ptr<const inst_lib_t> _ilib, const inst_seq_t & _prog=inst_seq_t())
+        : inst_lib(_ilib), program(_prog){ ; }
+      
+      Program(const Program &) = default;
+
+      void Clear() { program.clear(); }
+
+      Instruction & operator[](size_t id) { 
+        emp_assert(id < program.size());
+        return program[id]; 
+      }
+
+      const Instruction & operator[](size_t id) const {
+        emp_assert(id < program.size());
+        return program[id];
+      }
+
+      bool operator==(const Program & in) const { return program == in.program; }
+      bool operator!=(const Program & in) const { return !(*this == in); }
+      bool operator<(const Program & other) const { return program < other.program; }
+
+      /// Get program size.
+      size_t GetSize() const { return program.size(); }
+
+      emp::Ptr<const inst_lib_t> GetInstLibPtr() const { return inst_lib; }
+      const inst_lib_t & GetInstLib() const { return *inst_lib; }
+
+      bool ValidPosition(size_t pos) const { return pos < GetSize(); }
+
+
 
     };
 
@@ -346,12 +415,14 @@ namespace TagLGP {
 
     MemoryPosition default_mem;
 
+    size_t mem_size;
     emp::vector<tag_t> shared_mem_tags;
     emp::vector<tag_t> working_mem_tags;
     emp::vector<tag_t> input_mem_tags;
     emp::vector<tag_t> output_mem_tags;
 
     memory_t shared_mem;
+
     emp::vector<CallState> call_stack;
 
     size_t max_call_depth;
@@ -361,9 +432,65 @@ namespace TagLGP {
 
   public:
 
-  TagLinearGP_TW() { ; }
+  TagLinearGP_TW(emp::Ptr<const inst_lib_t> _ilib,
+                 emp::Ptr<emp::Random> rnd=nullptr)
+    : random_ptr(rnd), random_owner(false),
+      program(_ilib),
+      modules(),
+      default_mem(),
+      mem_size(DEFAULT_MEM_SIZE),
+      shared_mem_tags(mem_size),
+      working_mem_tags(mem_size),
+      input_mem_tags(mem_size),
+      output_mem_tags(mem_size),
+      shared_mem(mem_size),
+      call_stack(),
+      max_call_depth(DEFAULT_MAX_CALL_DEPTH),
+      min_tag_specificity(DEFAULT_MIN_TAG_SPECIFICITY),
+      is_executing(false)
+  { 
+    // If no random number generator is provided, create one (taking ownership).
+    if (!rnd) NewRandom(); 
+  }
+
+  TagLinearGP_TW(inst_lib_t & _ilib, 
+                 emp::Ptr<emp::Random> rnd=nullptr)
+    : TagLinearGP_TW(&_ilib, rnd) { ; }
+  
+  TagLinearGP_TW(const hardware_t & in) 
+    : random_ptr(nullptr), random_owner(false),
+      program(in.program),
+      modules(in.modules),
+      default_mem(in.default_mem),
+      mem_size(in.mem_size),
+      shared_mem_tags(in.shared_mem_tags),
+      working_mem_tags(in.working_mem_tags),
+      input_mem_tags(in.input_mem_tags),
+      output_mem_tags(in.output_mem_tags),
+      shared_mem(in.shared_mem),
+      call_stack(in.call_stack),
+      max_call_depth(in.max_call_depth),
+      min_tag_specificity(in.min_tag_specificity),
+      is_executing(in.is_executing)
+  {
+    if (in.random_owner) NewRandom();
+    else random_ptr = in.random_ptr;
+  }
+
+  ~TagLinearGP_TW() { if (random_owner) random_ptr.Delete(); }
 
 
+  void Reset();
+  void ResetHardware();
+
+  // ---- Hardware utilities ----
+  void NewRandom(int seed=-1) {
+    if (random_owner) random_ptr.Delete();
+    else random_ptr = nullptr;
+    random_ptr = emp::NewPtr<emp::Random>(seed);
+    random_owner = true;
+  }
+  
   };
 
 }
