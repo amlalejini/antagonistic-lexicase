0e8a15ac599f04d06dd5fc34563d1d2ded6b46d6
Alex Lalejini <amlalejini@gmail.com>
2018-12-19 09:15:13 -0500

    Update count prog synth solutions script.

--- scripts/countProgSynthSolutionsFound.py
+++ scripts/countProgSynthSolutionsFound.py
@@ -21,19 +21,24 @@ info_by_treatment = {}
 for sol in solutions:
     treatment = sol[header_lu["treatment"]]
     if not treatment in info_by_treatment:
-        info_by_treatment[treatment] = {"solutions_found":0, "total_runs":0}
+        info_by_treatment[treatment] = {"solutions_found":0, "total_runs":0, "min_solution_length":"NONE"}
     if (sol[header_lu["solution_found"]] == "True"):
         info_by_treatment[treatment]["solutions_found"] += 1
+        if info_by_treatment[treatment]["min_solution_length"] == "NONE":
+            info_by_treatment[treatment]["min_solution_length"] = sol[header_lu["solution_length"]]
+        elif info_by_treatment[treatment]["min_solution_length"] > sol[header_lu["solution_length"]]:
+            info_by_treatment[treatment]["min_solution_length"] = sol[header_lu["solution_length"]]
+
     info_by_treatment[treatment]["total_runs"] += 1
     info_by_treatment[treatment]["uses_cohorts"] = sol[header_lu["uses_cohorts"]]
     info_by_treatment[treatment]["problem"] = sol[header_lu["problem"]]
 
-solutions_summary = "treatment,test_mode,problem,uses_cohorts,solutions_found,total_runs\n"
+solutions_summary = "treatment,test_mode,problem,uses_cohorts,solutions_found,total_runs,min_solution_size\n"
 for treatment in info_by_treatment:
     info = info_by_treatment[treatment]
     print(treatment)
     test_mode = treatment.split("__")[1].replace("TESTS_", "")
-    solutions_summary += ",".join(map(str, [treatment, test_mode, info["problem"], info["uses_cohorts"], info["solutions_found"], info["total_runs"]])) + "\n"
+    solutions_summary += ",".join(map(str, [treatment, test_mode, info["problem"], info["uses_cohorts"], info["solutions_found"], info["total_runs"], info["min_solution_length"]])) + "\n"
 
 new_name = fpath.split("/")[-1].strip(".csv") + "__solutions_summary.csv"
 with open(new_name, "w") as fp:
